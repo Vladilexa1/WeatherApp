c9047e4cfa78a7fe5cabba2a932b8bc2a90a23bf
Vladilexa1 <vlad.slip4enko2012@gmail.com>
2024-07-26 12:28:15 +0300

    Create: user domain model

--- WeatherApp.Core/User.cs
+++ WeatherApp.Core/User.cs
@@ -2,14 +2,20 @@
 {
     public class User
     {
-        public User(int id, string login, string password)
+        private User(int id, string login, string passwordHash)
         {
             Id = id;
             Login = login;
-            Password = password;
+            PasswordHash = passwordHash;
         }
         public int Id { get; }
         public string Login { get; } = string.Empty;
-        public string Password { get; } = string.Empty;
+        public string PasswordHash { get; } = string.Empty;
+
+        public static User Create(int id, string login, string passwordHash)
+        {
+            var user = new User(id, login, passwordHash);
+            return user;
+        }
     }
 }
