bb1154d6366ed071e760db8407a25399e3f6ed05
Vladilexa1 <vlad.slip4enko2012@gmail.com>
2024-07-26 12:18:18 +0300

    Добавьте файлы проекта.

---
+++ WeatherApp.Core/User.cs
@@ -0,0 +1,15 @@
+﻿namespace WeatherApp.Core
+{
+    public class User
+    {
+        public User(int id, string login, string password)
+        {
+            Id = id;
+            Login = login;
+            Password = password;
+        }
+        public int Id { get; }
+        public string Login { get; } = string.Empty;
+        public string Password { get; } = string.Empty;
+    }
+}
