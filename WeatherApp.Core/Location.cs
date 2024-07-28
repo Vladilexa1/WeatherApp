2a6cdfaa31e96488110dbde6e760cec09abdf16c
Vladilexa1 <vlad.slip4enko2012@gmail.com>
2024-07-28 16:44:55 +0300

    Add: new model location

---
+++ WeatherApp.Core/Location.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp.Core
+{
+    public class Location
+    {
+        private Location(int id, string name, int userId, decimal latitude, decimal lonngitude)
+        {
+            Id = id;
+            Name = name;
+            UserId = userId;
+            Latitude = latitude;
+            Longitude = lonngitude;
+        }
+        public int Id { get; } = 0;
+        public string Name { get; } = string.Empty;
+        public int UserId { get; } = 0;
+        public decimal Latitude { get; }
+        public decimal Longitude { get; }
+
+        public static Location Create(int id, string name, int userId, decimal latitude, decimal lonngitude)// TODO: validation
+        {
+            Location location = new Location(id, name, userId, latitude, lonngitude);
+            return location;
+        }
+
+    }
+}
