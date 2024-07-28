c23b220d40378ab05cb5f580febfe8b5d1d65fa4
Vladilexa1 <vlad.slip4enko2012@gmail.com>
2024-07-28 16:52:31 +0300

    Add: new model Sessions

---
+++ WeatherApp.Core/Sessions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp.Core
+{
+    public class Sessions
+    {
+        private Sessions(Guid id, int userId, DateTime expiresAt)
+        {
+            Id = id;
+            UserId = userId;
+            ExpiresAt = expiresAt;
+        }
+        public Guid Id { get; }
+        public int UserId { get; }
+        public DateTime ExpiresAt { get; }
+        public static Sessions Create(Guid id, int userId, DateTime expiresAt) // TODO: validation
+        {
+            var Sessions = new Sessions(id, userId, expiresAt);
+            return Sessions;
+        }
+    }
+}
