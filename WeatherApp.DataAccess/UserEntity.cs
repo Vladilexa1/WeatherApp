d99a5de7e6a14120f966b1f17250d75c1e38ad5b
Vladilexa1 <vlad.slip4enko2012@gmail.com>
2024-07-28 17:23:44 +0300

    Add: DbContext, db entitys

---
+++ WeatherApp.DataAccess/UserEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace WeatherApp.DataAccess
+{
+    public class UserEntity
+    {
+        public int Id { get; }
+        public string Login { get; } = string.Empty;
+        public string PasswordHash { get; } = string.Empty;
+    }
+}
---
+++ WeatherApp.DataAccess/WeatherAppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System.Numerics;
+
+namespace WeatherApp.DataAccess
+{
+    public class WeatherAppDbContext(DbContextOptions<WeatherAppDbContext> options) : DbContext(options)
+    {
+        public DbSet<UserEntity> Users { get; set; }
+        public DbSet<LocationEntity> Locations { get; set; }
+        public DbSet<SessionEntity> Sessions { get; set; }
+
+        
+    }
+}
