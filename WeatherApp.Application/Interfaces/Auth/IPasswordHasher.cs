250e5e270f376991af52a53d35bad1c0ff0ae8f9
Vladilexa1 <vlad.slip4enko2012@gmail.com>
2024-08-03 16:24:24 +0300

    Add: interface passwordHesher

---
+++ WeatherApp.Application/Interfaces/Auth/IPasswordHasher.cs
@@ -0,0 +1,8 @@
+﻿namespace WeatherApp.Infrastructure
+{
+    public interface IPasswordHasher
+    {
+        string Generate(string password);
+        public bool Verify(string password, string passwordHash);
+    }
+}
