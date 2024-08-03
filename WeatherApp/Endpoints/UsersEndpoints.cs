975317f61062d8e875f27c34b84e4f6c0493b3d0
Vladilexa1 <vlad.slip4enko2012@gmail.com>
2024-08-03 16:22:36 +0300

    Add: Users endpoints

---
+++ WeatherApp/Endpoints/UsersEndpoints.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WeatherApp.Application;
+using WeatherApp.DataAccess.Repositories;
+
+namespace WeatherApp.API.Endpoints
+{
+    public static class UsersEndpoints 
+    {
+        public static IEndpointRouteBuilder MapUsersEndpoints(this IEndpointRouteBuilder app)
+        {
+            app.MapPost("register", Register);
+            app.MapPost("login", Login);
+            return app;
+        }
+        private static async Task<IResult> Register(string email, string password, IUserService userServices)
+        {
+            await userServices.Register(email, password);
+            return Results.Ok();
+        }
+        private static async Task<IResult> Login(string email, string password, IUserService userServices)
+        {
+            var token = userServices.Login(email, password);
+
+            return Results.Ok(token);
+        }
+    }
+}
--- WeatherApp/Program.cs
+++ WeatherApp/Program.cs
@@ -1,5 +1,10 @@
+using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
+using WeatherApp.API.Endpoints;
+using WeatherApp.Application;
 using WeatherApp.DataAccess;
+using WeatherApp.DataAccess.Repositories;
+using WeatherApp.Infrastructure;
 
 namespace WeatherApp
 {
@@ -10,20 +15,31 @@ namespace WeatherApp
             var builder = WebApplication.CreateBuilder(args);
 
             builder.Services.AddControllers();
-            
+            builder.Services.AddEndpointsApiExplorer();
+
             var configuration = builder.Configuration;
+            
+            builder.Services.AddSwaggerGen();
+
             builder.Services.AddDbContext<WeatherAppDbContext>(options =>
             {
                 options.UseNpgsql(configuration.GetConnectionString(nameof(WeatherAppDbContext)));
             });
-            
+
+
+            builder.Services.AddScoped<IUserRepository, UserRepository>();
+            builder.Services.AddScoped<IJWTProvider, JWTProvider>();
+            builder.Services.AddScoped<IPasswordHasher, PasswordHasher>();
+            builder.Services.AddScoped<IUserService, UserService>();
+
             var app = builder.Build();
 
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
-
-
+            app.UseSwagger();
+            app.UseSwaggerUI();
+            app.MapUsersEndpoints();
             app.MapControllers();
 
             app.Run();
