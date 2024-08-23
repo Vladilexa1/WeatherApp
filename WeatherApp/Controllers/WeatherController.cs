4893d1742e51179cfc841d754280bbd56119cb3d
Vladilexa1 <vlad.slip4enko2012@gmail.com>
2024-08-23 15:48:54 +0300

    Add: OpenApi documentation

--- WeatherApp/Controllers/WeatherController.cs
+++ WeatherApp/Controllers/WeatherController.cs
@@ -10,8 +10,20 @@ namespace WeatherApp.API.Controllers
 {
     [ApiController]
     [Route("Weather")]
+#pragma warning disable CS1591 // Отсутствует комментарий XML для открытого видимого типа или члена
     public class WeatherController : Controller
     {
+        /// <summary>
+        /// Search current weather in city by its name
+        /// </summary>
+        /// <param name="city"></param>
+        /// <param name="weatherService"></param>
+        /// <response code="400">If write wrong coordinates</response>
+        /// <response code="401">If user unauthorized</response>
+        /// <response code="404">If city is not faund</response>
+        /// <response code="503">If service is not available</response>
+        /// <response code="500">If have problem in server</response>
+        /// <returns></returns>
         [Authorize]
         [HttpGet("SearchLocation")]
         public async Task<ActionResult> SearchForName(string city, IWeatherService weatherService)
@@ -40,34 +52,42 @@ namespace WeatherApp.API.Controllers
 
             return Ok(currentWeather);
         }
+        /// <summary>
+        /// Add location to user
+        /// </summary>
+        /// <param name="latitude"></param>
+        /// <param name="longitude"></param>
+        /// <param name="weatherService"></param>
+        /// <response code="400">If write wrong coordinates</response>
+        /// <response code="401">If user unauthorized</response>
+        /// <response code="404">If city is not faund</response>
+        /// <response code="503">If service is not available</response>
+        /// <response code="500">If have problem in server</response>
+        /// <returns></returns>
         [Authorize]
         [HttpGet("Forecast")]
         public async Task<ActionResult> GetForecast(decimal latitude, decimal longitude, IWeatherService weatherService)
         {
-            //if (Authorize == false)
-            //{
-            //    Redirect(Login());
-            //}
             List<Forecast> response;
             try
             {
                 response = await weatherService.GetForecast(latitude, longitude);
             }
-            catch (WrongCoordinatesException)
+            catch (WrongCoordinatesException ex)
             {
-                return StatusCode(400, "Wrong Coordinates");
+                return StatusCode(400, ex.Message);
             }
-            catch (CityNotFaundException)
+            catch (CityNotFaundException ex)
             {
-                return StatusCode(404, "City not faund");
+                return StatusCode(404, ex.Message);
             }
-            catch (ServiceNotAvailableException)
+            catch (ServiceNotAvailableException ex)
             {
-                return StatusCode(503, "Service Unavailable");
+                return StatusCode(503, ex.Message);
             }
-            catch (Exception)
+            catch (Exception ex)
             {
-                return StatusCode(500, "Internal Server Error");
+                return StatusCode(500, ex.Message);
             }
             return Ok(response);
         }
